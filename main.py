b494cd93bd6080380730dd1eb7c03bda3bbe5f1b
Archit chakraborty <architchakraborty47@gmail.com>
2026-08-14 13:52:47 +0530

    basic minimal chatbot

--- main.py
+++ main.py
@@ -3,3 +3,25 @@ from pathlib import Path
 
 BACKEND_SRC = Path(__file__).parent / "Backend" / "src"
 sys.path.insert(0, str(BACKEND_SRC))
+
+from agent.main_llm import message_out
+
+while True:
+    chat_input = input("User: ")
+    
+    if chat_input.lower() in ["exit jarvis", "quit jarvis", "that's all for today jarvis"]:
+        print("Exiting the chat...")
+        break
+
+    msg = message_out.invoke(
+        {
+            "messages": [
+                {"role": "user", "content": chat_input}
+            ]
+            
+        },
+        config = {"configurable" : {
+            "thread_id" : "chat_thread",
+        }},
+        )
+    print(msg["messages"][-1].content)
